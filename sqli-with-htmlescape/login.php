0501a4e495cf0ff9e9d57edab2d74aaf5a6af851
Hiroshi Tokumaru <github@tokumaru.org>
2021-04-30 09:05:06 +0900

    SQL文表示を常時するように修正

--- sqli-with-htmlescape/login.php
+++ sqli-with-htmlescape/login.php
@@ -1,4 +1,5 @@
-<?php
+<html>
+<body><?php
   require_once 'mysqluser.php';
   header('Content-Type: text/html; charset=UTF-8');
   $id = htmlspecialchars($_GET['id'], ENT_QUOTES, 'UTF-8');    // ユーザID
@@ -7,11 +8,8 @@
   $dbh = new PDO('mysql:dbname=test', MYUSER, MYPASS);
   // SQLの組み立て
   $sql = "SELECT * FROM users WHERE id ='$id' AND pwd = '$pwd'";
-  $stmt = $dbh->query($sql);  // クエリー実行
-?>
-<html>
-<body><?php
   echo 'sql= ' . htmlspecialchars($sql, ENT_NOQUOTES, 'UTF-8') . '<br>';
+  $stmt = $dbh->query($sql);  // クエリー実行
   if ($stmt->rowCount() > 0) { // SELECTした行が存在する場合ログイン成功
     echo 'ログイン成功です';
   } else {
